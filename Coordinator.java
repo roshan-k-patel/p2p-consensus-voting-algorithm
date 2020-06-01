8517f631e2c2190a883fa6ca33b3419693f9e1e8
Roshan Patel <rp10g17@soton.ac.uk>
2020-06-01 15:34:07 +0800

    established connections between participant sockets and coordinator serversocket with args

--- Coordinator.java
+++ Coordinator.java
@@ -1,2 +1,106 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+
 public class Coordinator {
+
+    int currentParticipants = 0;
+    int expectedParticipants;
+    ArrayList<String> votingOptions;
+    int listenPort;
+    int timeout;
+    int coordinatorPort;
+    int participantStartPort = 12346;
+
+    private Coordinator(String[] args) throws InsufficientArgsException {
+
+        // java Coordinator <port> <lport> <parts> <timeout> [option]
+        //java Participant <cport> <lporti> <pport> <timeout>
+
+
+        if (args.length < 5) {
+            System.out.println("Minimum 5 arguments <port> <lport> <parts> <timeout> [option] ");
+            throw new InsufficientArgsException(args);
+        }
+        coordinatorPort = Integer.parseInt(args[0]);
+        listenPort = Integer.parseInt(args[1]);
+        expectedParticipants = Integer.parseInt(args[2]);
+        timeout = Integer.parseInt(args[3]);
+
+        // adds the voting options to an arraylist of Strings
+        votingOptions = new ArrayList<>();
+        for(int i = 4; i< args.length; i++) {
+            votingOptions.add(args[i]);
+        }
+
+        try {
+            //creates a serversocket for the coordinator
+            ServerSocket ss = new ServerSocket(coordinatorPort);
+            System.out.println("A Coordinator has been established on port: " + coordinatorPort);
+
+            //creates the correct number of participants with the necessary information. using the participant start port and incrementing it each creation
+            for (int i = 0; i < expectedParticipants ; i++ ) {
+                // TODO put participant parameters here
+                Participant participant = new Participant(coordinatorPort,listenPort,participantStartPort,timeout); // cport lport pport timeout
+                participantStartPort++;
+                try {
+                    Socket partSocket = ss.accept();
+                    currentParticipants++;
+                    new Thread(new Coordinator.ServiceThread(partSocket)).start();
+                } catch (Exception e) {
+                    System.out.println("error " + e);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("error " + e);
+        }
+
+        try {
+            Thread.sleep(500);
+            System.out.println("All participants have joined. There are " + currentParticipants + " participants");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    static class ServiceThread implements Runnable {
+        Socket participant;
+
+
+        ServiceThread(Socket p) {
+            participant = p;
+        }
+
+        public void run() {
+            try {
+                BufferedReader in = new BufferedReader(new InputStreamReader(participant.getInputStream()));
+                String line;
+                while ((line = in.readLine()) != null) System.out.println(line + " received");
+                // participant.close();
+            } catch (Exception e) {
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            Coordinator coordinator = new Coordinator(args);
+            //Waits for all participants to connect
+        } catch (InsufficientArgsException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static class InsufficientArgsException extends Exception {
+        String[] args;
+
+        InsufficientArgsException(String[] args) {
+            this.args = args;
+        }
+
+    }
 }
--- Participant.java
+++ Participant.java
@@ -1,2 +1,39 @@
-public class Participant extends Thread{
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class Participant {
+
+    int coordinatorPort;
+    int listenerPort;
+    int participantport;
+    int timeout;
+
+    public Participant(int cport, int lport, int pport, int timeout) {
+
+        this.coordinatorPort = cport;
+        this.listenerPort = lport;
+        this.participantport = pport;
+        this.timeout = timeout;
+
+        try {
+            // Creates a socket with host ip address and port
+            Socket socket = new Socket(InetAddress.getLocalHost(), coordinatorPort);
+
+/*            Printer writer which prints a message into the socket output stream and then flushes the stream
+            and a sleeps for 1 second*/
+
+            PrintWriter out = new PrintWriter(socket.getOutputStream());
+            out.println("JOIN " + this.participantport);
+            out.flush();
+           /* for (int i = 0; i < 10; i++) {
+                out.println("TCP message " + i);
+                out.flush();
+                System.out.println("TCP message " + i + " sent");
+                Thread.sleep(1000);
+            }*/
+        } catch (Exception e) {
+            System.out.println("error" + e);
+        }
+    }
 }
