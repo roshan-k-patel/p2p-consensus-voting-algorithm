9f5a0fb3e4e753fdfc7ac35e86d409a1c14f39dc
Roshan Patel <rp10g17@soton.ac.uk>
2020-06-02 13:46:48 +0800

    Sent and received details of other participants and vote options

--- Coordinator.java
+++ Coordinator.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -18,7 +16,7 @@ public class Coordinator {
     int timeout;
     int coordinatorPort;
     int participantStartPort = 12346;
-    static ArrayList<Integer> participantPorts  = new ArrayList<>();
+    static ArrayList<Integer> participantPorts = new ArrayList<>();
 
     private Coordinator(String[] args) throws InsufficientArgsException {
 
@@ -37,7 +35,7 @@ public class Coordinator {
 
         // adds the voting options to an arraylist of Strings
         votingOptions = new ArrayList<>();
-        for(int i = 4; i< args.length; i++) {
+        for (int i = 4; i < args.length; i++) {
             votingOptions.add(args[i]);
         }
 
@@ -48,9 +46,9 @@ public class Coordinator {
             System.out.println("A Coordinator has been established on port: " + coordinatorPort);
 
             //creates the correct number of participants with the necessary information. using the participant start port and incrementing it each creation
-            while (currentParticipants < expectedParticipants){
+            while (currentParticipants < expectedParticipants) {
                 // create participant object with parameters here
-                Participant participant = new Participant(coordinatorPort,listenPort,participantStartPort,timeout); // cport lport pport timeout
+                Participant participant = new Participant(coordinatorPort, listenPort, participantStartPort, timeout); // cport lport pport timeout
 
                 try {
                     //creates a new socket from the accepted server connection, and then starts a new thread with this socket (participant connects directly with this socket)
@@ -58,7 +56,7 @@ public class Coordinator {
                     //adds the connected participant port to the arraylist of ports
                     participantStartPort++;
                     currentParticipants++;
-                    new Thread(new CoordinatorServer(partSocket, this)).start();
+                    new Thread(new CoordinatorServerThread(partSocket, this)).start();
                 } catch (Exception e) {
                     System.out.println("error " + e);
                 }
@@ -79,29 +77,29 @@ public class Coordinator {
 
     }
 
-    public int getParticipantListSize(){
+    public int getParticipantListSize() {
         return participantPorts.size();
     }
 
-    public synchronized String getParticipantListString(){
+    public synchronized String getParticipantListString() {
         String partList = "";
-        for (Integer x : participantPorts){
+        for (Integer x : participantPorts) {
             partList = partList + x + " ";
         }
 
         return partList;
     }
 
-    public synchronized void addParticipant(Integer port){
+    public synchronized void addParticipant(Integer port) {
         participantPorts.add(port);
     }
 
     // runs coordinator server connections on their own thread, keep open for any communication rec from corresponding participant
-    class CoordinatorServer implements Runnable {
+    class CoordinatorServerThread implements Runnable {
         Socket participant;
         Coordinator coordinator;
 
-        CoordinatorServer(Socket p, Coordinator c) {
+        CoordinatorServerThread(Socket p, Coordinator c) {
             participant = p;
             coordinator = c;
         }
@@ -109,13 +107,13 @@ public class Coordinator {
         public void run() {
             try {
                 BufferedReader in = new BufferedReader(new InputStreamReader(participant.getInputStream()));
-                PrintWriter out = new PrintWriter(participant.getOutputStream());
+                PrintWriter out = new PrintWriter(participant.getOutputStream(), true);
                 String line;
                 String sPort = "";
                 int port;
                 while ((line = in.readLine()) != null) {
                     // if the message is a join message then splits the string and adds the port to the static arraylist
-                    if (line.contains("JOIN")){
+                    if (line.contains("JOIN")) {
                         String[] parts = line.split(" ");
                         sPort = parts[1];
                         port = Integer.parseInt(sPort);
@@ -131,17 +129,31 @@ public class Coordinator {
                     String detailsSpecific = "";
 
                     // removes own port from the list of ports and
-                    if (details.contains(sPort)){
-                        detailsSpecific = details.replace(sPort,"");
+                    if (details.contains(sPort)) {
+                        detailsSpecific = details.replace(sPort, "");
                         detailsSpecific = detailsSpecific.trim();
                         detailsSpecific = detailsSpecific.replace("  ", " ");
-                        System.out.println(detailsSpecific);
+                        detailsSpecific = "DETAILS " + detailsSpecific;
                     }
 
                     Thread.sleep(500);
-                    // TODO SENDS THE PARTICIPANT DETAILS
+                    // SENDS THE PARTICIPANT DETAILS
                     out.println(detailsSpecific);
-                    System.out.println("Sent: " + detailsSpecific);
+                    System.out.println("Sent Details: " + detailsSpecific);
+
+                    Thread.sleep(2500);
+
+                    // SENDS THE VOTING OPTIONS
+                    String votingOptions = "";
+                    for (String x : coordinator.votingOptions) {
+                        votingOptions = votingOptions + x + " ";
+                    }
+                    votingOptions = "VOTE_OPTIONS " + votingOptions;
+
+                    System.out.println("Sending Vote options: " + votingOptions);
+
+                    out.println(votingOptions);
+
                 }
 
                 // participant.close();
@@ -168,7 +180,7 @@ public class Coordinator {
         }
     }
 
-    public void testMethod(){
+    public void testMethod() {
 
     }
 }
--- Participant.java
+++ Participant.java
@@ -16,6 +16,8 @@ public class Participant {
     ArrayList<String> votingOptions;
     String myvote;
     ArrayList<String> otherParticipants;
+    boolean hasDetails;
+    boolean hasVoteOptions;
 
     public Participant(int cport, int lport, int pport, int timeout) {
 
@@ -24,23 +26,18 @@ public class Participant {
         this.participantport = pport;
         this.timeout = timeout;
         votingOptions = new ArrayList<>();
-        boolean hasDetails = false;
+        hasDetails = false;
+        hasVoteOptions = false;
 
         try {
             // Creates a socket with host ip address and coordinator port
             Socket socket = new Socket(InetAddress.getLocalHost(), coordinatorPort);
 
-            // SENDING JOIN REQUEST AND PORT ID TO COORDINATOR
-            // Printer writer which prints a message into the socket output stream
-            PrintWriter out = new PrintWriter(socket.getOutputStream());
-            out.println("JOIN " + this.participantport);
-            out.flush();
 
-            // RECEIVING PARTICIPANT DETAILS FROM COORDINATOR
-            // Buffered reader which reads a message from the input stream
+            new Thread(new Participant.ParticipantListenerThread(socket, this)).start();
+
+
             otherParticipants = new ArrayList<>();
-
-            // TRY TO HAVE A LISTENING THREAD LIKE WITH COORDINATOR
 
 
         } catch (Exception e) {
@@ -50,32 +47,78 @@ public class Participant {
 
     }
 
-    private synchronized void readDetails(){
+    public synchronized void addOtherParticipant(String participant) {
+        this.otherParticipants.add(participant);
+    }
 
+    public synchronized void addVotingOption(String option) {
+        this.votingOptions.add(option);
+    }
+
+
+    static class ParticipantListenerThread implements Runnable {
+        Socket partServer;
+        Participant participant;
+
+        ParticipantListenerThread(Socket s, Participant p) {
+            partServer = s;
+            participant = p;
+        }
+
+        public void run() {
+            try {
+                BufferedReader in = new BufferedReader(new InputStreamReader(partServer.getInputStream()));
+                PrintWriter out = new PrintWriter(partServer.getOutputStream(), true);
+
+                // SENDING JOIN REQUEST AND PORT ID TO COORDINATOR
+                // Printer writer which prints a message into the socket output stream
+                out.println("JOIN " + participant.participantport);
+
+                Thread.sleep(2000);
+
+                // ACCEPTING OTHER PARTICIPANTS (DETAILS) AND VOTE OPTIONS
+                String line;
+                while ((line = in.readLine()) != null) {
+
+                    //DETAILS
+                    if (line.contains("DETAILS")) {
+                        String[] parts = line.split(" ");
+
+                        for (int i = 1; i < parts.length; i++) {
+                            participant.addOtherParticipant(parts[i]);
+                        }
+                        participant.hasDetails = true;
+                        System.out.println("received details");
+                    }
+
+                    //VOTE_OPTIONS
+                    if (line.contains("VOTE_OPTIONS")) {
+                        String[] parts = line.split(" ");
+
+                        for (int i = 1; i < parts.length; i++) {
+                            participant.addVotingOption(parts[i]);
+                        }
+
+                        participant.hasVoteOptions = true;
+                        System.out.println("Received voting options");
+
+                        if (participant.hasVoteOptions && participant.hasDetails) {
+                            break;
+                        }
+
+                    }
+                }
+                Thread.sleep(500);
+                System.out.println("Vote is ready to commence");
+                // participant.close();
+            } catch (Exception e) {
+            }
+        }
     }
 
     private String pickMyVote() {
         Random rand = new Random();
         int x = rand.nextInt(votingOptions.size());
         return votingOptions.get(x);
-    }
-
-    static class ParticipantServer implements Runnable {
-        Socket partServer;
-
-        ParticipantServer(Socket p) {
-            partServer = p;
-        }
-
-        public void run() {
-            try {
-                //TODO check the string to see if it's details or options. If it's details do something, if it's options do something else
-                BufferedReader in = new BufferedReader(new InputStreamReader(partServer.getInputStream()));
-                String line;
-                while ((line = in.readLine()) != null) System.out.println(line + " received");
-                // participant.close();
-            } catch (Exception e) {
-            }
-        }
     }
 }
